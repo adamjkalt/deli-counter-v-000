df93f952a2a070ad9f60493ec605ed11484bd76a
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-12 17:04:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.size < 1
     puts "The line is currently empty."
-  elsif katz_deli.size > 1
+  elsif katz_deli.size < 4
     puts "The line is currently #{other_deli}."
-  elsif katz_deli.size > 3
+  elsif katz_deli.size > 4
     puts "The line is currently #{another_deli}."
   end
 end
