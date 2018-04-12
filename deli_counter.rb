9309442e7aab6b24a3cd326dc21cd5d3960f3675
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-12 19:14:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
-def now_serving(katz_deli, name)
+def now_serving(katz_deli)
   if katz_deli.size > 0
     katz_deli.unshift(name)
     puts "Currently serving #{name}"
