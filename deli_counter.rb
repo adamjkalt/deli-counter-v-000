34c57a0fb07401a2d1cf17d4dbef39dabaf609ba
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-12 19:14:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ end
 
 def now_serving(katz_deli, name)
   if katz_deli.size > 0
-    katz_deli.unshift
+    katz_deli.unshift(name)
     puts "Currently serving #{name}"
   else
     puts "There is nobody waiting to be served!"
