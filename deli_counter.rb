ebc23a88c6dda07eec7f01ae455eaf6b6dac5b76
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-12 18:28:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,7 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number 1 in line."
   elsif katz_deli.size > 1
     katz_deli.push("#{name}")
+    puts "Welcome, #{name}. You are number 4 in line."
   end
 end
 
